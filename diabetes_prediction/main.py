39bef1bf0cd1d1d1e1f880270a2c17848c8916fa
Kanishk-32 <kanishkg080@gmail.com>
2026-03-28 21:06:55 +0530

    Trained model

--- diabetes_prediction/main.py
+++ diabetes_prediction/main.py
@@ -17,3 +17,24 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#calling for  model training
+
+from src.utils import load_config, save_model
+from src.data_preprocessing import load_data, clean_data
+from src.model_training import train_model
+
+def main():
+    config = load_config()
+
+    df = load_data(config["data_path"])
+    df = clean_data(df)
+
+    model = train_model(df)
+
+    save_model(model, config["model_path"])
+
+    print("Model trained and saved successfully!")
+
+if __name__ == "__main__":
+    main()
