6bd644eba973d8c8686269fc31f12c876d963fe3
Kanishk-32 <kanishkg080@gmail.com>
2026-04-16 20:55:30 +0530

    working on landing page

--- app/app.py
+++ app/app.py
@@ -22,6 +22,10 @@ model = joblib.load(model_path)
 scaler = joblib.load(scaler_path)
 
 @app.route("/")
+def landing():
+    return render_template("landing.html")
+
+@app.route("/app")
 def index():
     return render_template("index.html")
 
